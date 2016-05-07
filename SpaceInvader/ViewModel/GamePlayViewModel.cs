a190623a288fe24a95094fe8b6f7bbec162f0fd0
Péter Zsolt <pezso9@msn.com>
2016-05-07 19:45:58 +0200

    asd

---
+++ SpaceInvader/ViewModel/GamePlayViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceInvader.Model;
+using System.Windows.Controls;
+
+namespace SpaceInvader.ViewModel
+{
+    class GamePlayViewModel
+    {
+        OptionsModel opt;
+        PlayerSpaceShip playerSpaceShip;
+
+        public GamePlayViewModel()
+        {
+            opt = new OptionsModel();
+            opt.optionsXmlDataProvider.getXmlData(opt);
+        }
+    }
+}
